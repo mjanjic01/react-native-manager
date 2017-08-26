07949ccecd9404223bd527ccd3a5afbfdb10a74f
Matej Janjic <matej.janjic@infinum.hr>
2017-08-26 22:30:51 +0200

    Add actions and action types

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+import firebase from 'firebase';
+
+import * as types from './types';
+
+export const emailChanged = (text) => {
+  return {
+    type: types.EMAIL_CHANGED,
+    payload: text
+  };
+};
+
+export const passwordChanged = (text) => {
+  return {
+    type: types.PASSWORD_CHANGED,
+    payload: text
+  };
+};
+
+export const loginUser = ({ email, password}) => {
+  return (dispatch) => {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(user => {
+          dispatch({
+            type: types.USER_LOGIN_SUCCESS,
+            payload: user
+          });
+        }).catch(error => {
+          dispatch({
+            type: types.USER_LOGIN_FAIL,
+            payload: error
+          });
+        });
+  };
+};
